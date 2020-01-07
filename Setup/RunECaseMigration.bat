631e741f91e07dd160a6159ef93ba33d4457505c
davidjamespugh <davidjames.pugh@gmail.com>
2020-01-07 14:08:20 +1300

    Updating batch file for script installation. Updating gitignore to ignore xlsx files

--- Setup/RunECaseMigration.bat
+++ Setup/RunECaseMigration.bat
@@ -1,4 +1,4 @@
-pushd \\SAV-FP01\data$\Shared Data\Care Manager\Quality Data\Data Technician\eCase Migration\
+pushd \\SAV-FP01\data$\Shared Data\Care Manager\Quality Data\Data Technician\eCase Migration\ecase_automation
 
 python __init__.py
 
